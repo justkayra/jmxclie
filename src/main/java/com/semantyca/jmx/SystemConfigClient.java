73916b028a705b356b3ebd5a8665d44540566b61
kaira.kuliyeva <kaira.kuliyeva@accenture.com>
2020-01-09 14:57:16 +0200

    #

--- src/main/java/com/semantyca/jmx/SystemConfigClient.java
+++ src/main/java/com/semantyca/jmx/SystemConfigClient.java
@@ -1,27 +1,23 @@
 package com.semantyca.jmx;
 
-import javax.management.InstanceAlreadyExistsException;
-import javax.management.JMX;
-import javax.management.MBeanServer;
 import javax.management.MBeanServerConnection;
-import javax.management.MBeanServerInvocationHandler;
 import javax.management.MalformedObjectNameException;
-import javax.management.ObjectInstance;
-import javax.management.ObjectName;
-import javax.management.StandardMBean;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
-import java.util.Map;
-import java.util.Set;
 
 public class SystemConfigClient {
+    static  String  host = "localhost", port = "9990";
 
     public static void main(String[] args) throws IOException, MalformedObjectNameException {
-        String host = "10.0.75.1";
-        int port = 7155;
+        try {
+            host = args[0];
+            port = args[1];
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("It will be a default connection");
+        }
+
         String urlString = "service:jmx:rmi:///jndi/rmi://" + host + ":" + port + "/jmxrmi";
         JMXServiceURL url = new JMXServiceURL(urlString);
         JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
